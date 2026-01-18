47af28bf4911e8f0be86a7bbd4744f72d065ecbd
bychenkv <bychenkov@proton.me>
2026-01-18 20:14:38 +0500

    feat: create template with java servlet

---
+++ src/main/java/com/bychenkv/CurrencyServlet.java
@@ -0,0 +1,21 @@
+package com.bychenkv;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/currencies")
+public class CurrencyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter pw = resp.getWriter();
+        pw.write("Hello!");
+        pw.close();
+    }
+}
