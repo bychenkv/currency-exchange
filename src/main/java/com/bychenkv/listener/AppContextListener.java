6b271c0696802bd129f55d80bb3f7412c1350ca6
bychenkv <bychenkov@proton.me>
2026-02-02 14:05:53 +0500

    fix: use env variable to store db path
    - move AppContextListener to listener package
    - add configureDataSource() and configureServices() helper methods in contextInitialized()

--- src/main/java/com/bychenkv/listener/AppContextListener.java
+++ src/main/java/com/bychenkv/listener/AppContextListener.java
@@ -1,4 +1,4 @@
-package com.bychenkv;
+package com.bychenkv.listener;
 
 import com.bychenkv.dao.CurrencyDao;
 import com.bychenkv.dao.ExchangeRateDao;
@@ -12,15 +12,35 @@ import jakarta.servlet.annotation.WebListener;
 import org.sqlite.SQLiteDataSource;
 import tools.jackson.databind.ObjectMapper;
 
+import javax.sql.DataSource;
+
 @WebListener
 public class AppContextListener implements ServletContextListener {
-    private final static String DB_CONNECTION_URL = "jdbc:sqlite:/Users/mac/currency.db";
+    private static final String SQLITE_PREFIX = "jdbc:sqlite:";
+    private static final String CURRENCY_DB_PATH_ENV = "CURRENCY_DB_PATH";
+    private static final String LOCAL_DB_PATH = "/Users/mac/currency.db";
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        SQLiteDataSource dataSource = new SQLiteDataSource();
-        dataSource.setUrl(DB_CONNECTION_URL);
+        DataSource dataSource = configureDataSource();
+        configureServices(sce, dataSource);
 
+        sce.getServletContext().setAttribute("mapper", new ObjectMapper());
+    }
+
+    private static DataSource configureDataSource() {
+        String dbPath = System.getenv(CURRENCY_DB_PATH_ENV);
+        if (dbPath == null) {
+            dbPath = LOCAL_DB_PATH;
+        }
+
+        SQLiteDataSource dataSource = new SQLiteDataSource();
+        dataSource.setUrl(SQLITE_PREFIX + dbPath);
+
+        return dataSource;
+    }
+
+    private static void configureServices(ServletContextEvent sce, DataSource dataSource) {
         CurrencyDao currencyDao = new CurrencyDao(dataSource);
         ExchangeRateDao exchangeRateDao = new ExchangeRateDao(dataSource);
 
@@ -28,12 +48,9 @@ public class AppContextListener implements ServletContextListener {
         CurrencyService currencyService = new CurrencyService(currencyDao);
         ExchangeRateService exchangeRateService = new ExchangeRateService(exchangeRateDao, currencyDao);
 
-        ObjectMapper mapper = new ObjectMapper();
-
         ServletContext context = sce.getServletContext();
         context.setAttribute("currencyService", currencyService);
         context.setAttribute("exchangeRateService", exchangeRateService);
         context.setAttribute("exchangeService", exchangeService);
-        context.setAttribute("mapper", mapper);
     }
 }
