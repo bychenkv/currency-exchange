d94064aff56a78d134c0abbb69367ff506cad559
bychenkv <bychenkov@proton.me>
2026-02-20 15:02:40 +0500

    refactor: remove constructor from String in CurrencyCodePair

--- src/main/java/com/bychenkv/model/CurrencyCodePair.java
+++ src/main/java/com/bychenkv/model/CurrencyCodePair.java
@@ -1,10 +1,6 @@
-package com.bychenkv.dto;
+package com.bychenkv.model;
 
 public record CurrencyCodePair(String base, String target) {
-    public CurrencyCodePair(String codePair) {
-        this(codePair.substring(0, 3), codePair.substring(3, 6));
-    }
-
     @Override
     public String toString() {
         return base + target;
